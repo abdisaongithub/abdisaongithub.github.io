c023ebeb931e4c8085b6c6e71299f620a2a5e3e6
Abdisa T. <abdtsegaye@gmail.com>
2024-04-22 21:46:05 +0300

    finished android UI, moving on to iOS

--- lib/src/android.dart
+++ lib/src/android.dart
@@ -20,7 +20,9 @@ class _AndroidUIState extends State<AndroidUI> {
       body: SingleChildScrollView(
         child: Container(
           height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
+          width: MediaQuery.of(context).orientation == Orientation.portrait
+              ? MediaQuery.of(context).size.width
+              : 400,
           decoration: const BoxDecoration(
             image: DecorationImage(
               fit: BoxFit.cover,
@@ -29,232 +31,241 @@ class _AndroidUIState extends State<AndroidUI> {
               ),
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(
-                height: 60,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  const AndroidIcon(
-                    image: 'assets/images/cloud.svg',
-                  ),
                   const SizedBox(
-                    width: 8,
+                    height: 60,
                   ),
-                  Text(
-                    '33°',
-                    style: jumboText,
-                  ),
-                ],
-              ),
-              Text(
-                'Partly Cloudy',
-                style: h3,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.location_on,
-                    color: CustomColor.whito,
-                    size: 18,
-                  ),
-                  const SizedBox(
-                    width: 6,
-                  ),
-                  Text(
-                    'Addis Ababa',
-                    style: h3,
-                  ),
-                ],
-              ),
-              Text(
-                'UV Index: Perfect',
-                style: h3,
-              ),
-              const Expanded(
-                child: SizedBox(
-                  height: 20,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 30,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        // AndroidIcon(
-                        //   image: 'assets/icons/android/phone.svg',
-                        //   svg: true,
-                        //   onTap: () {
-                        //     Launcher().launchUrl(
-                        //       context: context,
-                        //       url: 'https://google.com',
-                        //     );
-                        //   },
-                        // ),
-                        InkWell(
-                          onTap: () {
-                            Launcher().launchUrl(
-                              context: context,
-                              url: 'https://miltotech.com/abdi',
-                            );
-                          },
-                          child: const FlutterLogo(
-                            size: 50,
-                            style: FlutterLogoStyle.markOnly,
-                          ),
-                        ),
-                        Text(
-                          'Projects',
-                          style: textMedium,
-                        ),
-                      ],
-                    ),
-                    AndroidIconButton(
-                      image: 'assets/icons/chrome.svg',
-                      svg: true,
-                      text: 'Website',
-                      onTap: () {
-                        Launcher().launchUrl(
-                          context: context,
-                          url: 'https://miltotech.com',
-                        );
-                      },
-                    ),
-                    AndroidIconButton(
-                      image: 'assets/icons/pdf.svg',
-                      svg: true,
-                      text: 'CV',
-                      onTap: () {
-                        Launcher().launchUrl(
-                          context: context,
-                          url: 'https://github.com/abdisaongithub',
-                        );
-                      },
-                    ),
-                    AndroidIconButton(
-                      image: 'assets/icons/upwork.png',
-                      svg: false,
-                      text: 'UpWork',
-                      onTap: () {
-                        Launcher().launchUrl(
-                          context: context,
-                          url: 'https://www.upwork.com/freelancers/~01d8a688898b3f8808',
-                        );
-                      },
-                    ),
-                    AndroidIconButton(
-                      image: 'assets/icons/github.svg',
-                      svg: true,
-                      text: 'GitHub',
-                      onTap: () {
-                        Launcher().launchUrl(
-                          context: context,
-                          url: 'https://github.com/abdisaongithub',
-                        );
-                      },
-                      bgColor: Colors.white,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Container(
-                        color: Colors.white,
-                        height: 2,
-                        width: 8,
+                      const AndroidIcon(
+                        image: 'assets/images/cloud.svg',
                       ),
                       const SizedBox(
-                        height: 4,
-                      ),
-                      Container(
-                        color: Colors.white,
-                        height: 2,
                         width: 8,
+                      ),
+                      Text(
+                        '33°',
+                        style: jumboText,
                       ),
                     ],
                   ),
-                  const SizedBox(
-                    width: 8,
+                  Text(
+                    'Partly Cloudy',
+                    style: h3,
                   ),
-                  Container(
-                    height: 10,
-                    width: 10,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.white,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.location_on,
+                        color: CustomColor.whito,
+                        size: 18,
+                      ),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        'Addis Ababa',
+                        style: h3,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'UV Index: Perfect',
+                    style: h3,
+                  ),
+                  const Expanded(
+                    child: SizedBox(
+                      height: 20,
                     ),
                   ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Container(
-                    height: 10,
-                    width: 10,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            // AndroidIcon(
+                            //   image: 'assets/icons/android/phone.svg',
+                            //   svg: true,
+                            //   onTap: () {
+                            //     Launcher().launchUrl(
+                            //       context: context,
+                            //       url: 'https://google.com',
+                            //     );
+                            //   },
+                            // ),
+                            InkWell(
+                              onTap: () {
+                                Launcher().launchUrl(
+                                  context: context,
+                                  url: 'https://miltotech.com/abdi',
+                                );
+                              },
+                              child: const FlutterLogo(
+                                size: 50,
+                                style: FlutterLogoStyle.markOnly,
+                              ),
+                            ),
+                            Text(
+                              'Projects',
+                              style: textMedium,
+                            ),
+                          ],
+                        ),
+                        AndroidIconButton(
+                          image: 'assets/icons/chrome.svg',
+                          svg: true,
+                          text: 'Website',
+                          onTap: () {
+                            Launcher().launchUrl(
+                              context: context,
+                              url: 'https://miltotech.com',
+                            );
+                          },
+                        ),
+                        AndroidIconButton(
+                          image: 'assets/icons/pdf.svg',
+                          svg: true,
+                          text: 'CV',
+                          onTap: () {
+                            Launcher().launchUrl(
+                              context: context,
+                              url: 'https://github.com/abdisaongithub',
+                            );
+                          },
+                        ),
+                        AndroidIconButton(
+                          image: 'assets/icons/upwork.png',
+                          svg: false,
+                          text: 'UpWork',
+                          onTap: () {
+                            Launcher().launchUrl(
+                              context: context,
+                              url:
+                                  'https://www.upwork.com/freelancers/~01d8a688898b3f8808',
+                            );
+                          },
+                        ),
+                        AndroidIconButton(
+                          image: 'assets/icons/github.svg',
+                          svg: true,
+                          text: 'GitHub',
+                          onTap: () {
+                            Launcher().launchUrl(
+                              context: context,
+                              url: 'https://github.com/abdisaongithub',
+                            );
+                          },
+                          bgColor: Colors.white,
+                        ),
+                      ],
                     ),
                   ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            color: Colors.white,
+                            height: 2,
+                            width: 8,
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Container(
+                            color: Colors.white,
+                            height: 2,
+                            width: 8,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        height: 10,
+                        width: 10,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        height: 10,
+                        width: 10,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      AndroidIcon(
+                        image: 'assets/icons/android/calendar.svg',
+                      ),
+                      AndroidIcon(
+                        image: 'assets/icons/android/internet.svg',
+                      ),
+                      AndroidIcon(
+                        image: 'assets/icons/android/my-files.svg',
+                      ),
+                      AndroidIcon(
+                        image: 'assets/icons/android/phone.svg',
+                      ),
+                      AndroidIcon(
+                        image: 'assets/icons/android/planner.svg',
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  // Expanded(child: const SizedBox(height: 10,),),
+                  Center(
+                    child: Container(
+                      height: 5,
+                      width:
+                          MediaQuery.of(context).orientation == Orientation.portrait
+                              ? MediaQuery.sizeOf(context).width * 0.4
+                              : 180,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
                 ],
               ),
-              const SizedBox(height: 20),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 20,
-                  ),
-                  AndroidIcon(
-                    image: 'assets/icons/android/calendar.svg',
-                  ),
-                  AndroidIcon(
-                    image: 'assets/icons/android/internet.svg',
-                  ),
-                  AndroidIcon(
-                    image: 'assets/icons/android/my-files.svg',
-                  ),
-                  AndroidIcon(
-                    image: 'assets/icons/android/phone.svg',
-                  ),
-                  AndroidIcon(
-                    image: 'assets/icons/android/planner.svg',
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              // Expanded(child: const SizedBox(height: 10,),),
-              Center(
-                child: Container(
-                  height: 5,
-                  width: MediaQuery.sizeOf(context).width * 0.4,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 12),
             ],
           ),
         ),
--- lib/src/landing/landing_screen.dart
+++ lib/src/landing/landing_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio_app/src/android.dart';
 
@@ -13,6 +16,72 @@ class LandingScreen extends StatefulWidget {
 class _LandingScreenState extends State<LandingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const AndroidUI();
+    if (defaultTargetPlatform == TargetPlatform.linux) {
+      return const AndroidUI();
+    } else if (Platform.isIOS) {
+      return Scaffold(
+        body: Container(
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: const Center(
+            child: Text("iOS App"),
+          ),
+        ),
+      );
+    } else if (Platform.isWindows) {
+      return Scaffold(
+        body: Container(
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: const Center(
+            child: Text("Windows App"),
+          ),
+        ),
+      );
+    } else if (Platform.isLinux) {
+      return Scaffold(
+        body: Container(
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: const Center(
+            child: Text("Linux App"),
+          ),
+        ),
+      );
+    } else if (Platform.isMacOS) {
+      return Scaffold(
+        body: Container(
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: const Center(
+            child: Text("MacOS App"),
+          ),
+        ),
+      );
+    }
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        child: const Center(
+          child: Text("Could not find OS"),
+        ),
+      ),
+    );
   }
 }
