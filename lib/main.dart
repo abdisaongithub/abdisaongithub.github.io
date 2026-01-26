bd84fea536e4f73ccdec3a57d7989ff0411b70d7
Abdisa Tsegaye <48585612+abdisaongithub@users.noreply.github.com>
2026-01-26 22:27:43 +0300

    feat: integrate terminal with virtual file system
    Connects TerminalApp to FileSystemCubit to support ls, cd, mkdir, touch commands. Closes #5.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-// If desktop
+import 'features/boot/boot_screen.dart';
 import 'main_orchestrator.dart';
 import 'features/os_mode/cubit/os_mode_cubit.dart';
 import 'features/virtual_window/cubit/window_manager_cubit.dart';
+import 'features/theme/theme_cubit.dart';
+import 'features/file_system/cubit/file_system_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   runApp(const PortfolioApp());
 }
 
@@ -20,12 +21,25 @@ class PortfolioApp extends StatelessWidget {
       providers: [
         BlocProvider(create: (_) => OSModeCubit()),
         BlocProvider(create: (_) => WindowManagerCubit()),
+        BlocProvider(create: (_) => ThemeCubit()),
+        BlocProvider(create: (_) => FileSystemCubit()),
       ],
       child: const MaterialApp(
         title: 'Abdisa Portfolio',
         debugShowCheckedModeBanner: false,
-        home: MainOrchestrator(),
+        home: BootScreen(),
       ),
     );
+  }
+}
+
+// The core OS system, accessed after login
+class MainSystem extends StatelessWidget {
+  const MainSystem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Blocs are now provided at the root, so we just return the Orchestrator
+    return const MainOrchestrator();
   }
 }
